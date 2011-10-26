5588299861189031e067263cdf75cc067b61da02
Anders Hørsted <anders@alphabits.dk>
2011-10-26 04:25:18 +0200

    Deleted a bunch of files

--- 02610/assignment-1/calc_contourbranin.m
+++
@@ -1,3 +0,0 @@
-function [logfs] = calc_contourbranin(X, Y)
-    logfs = max(log10(arrayfun(@mybranin, X, Y)), -5);
-end
--- 02610/assignment-1/contourbranin.m
+++
@@ -1,7 +0,0 @@
-function contourbranin(X, Y, logfs, v)
-
-    contour(X, Y, logfs, v, 'linewidth', 1);
-    colorbar;
-    set(gca, 'fontsize', 14);
-
-end
